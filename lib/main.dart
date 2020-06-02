f45fa3c15dac5b452a07419ca8e36069cd1675cb
michael <michael@initsng.com>
2020-06-02 11:59:41 +0100

    Seperate widgets into classes

--- lib/main.dart
+++ lib/main.dart
@@ -65,89 +65,8 @@ RouteFactory _route(){
   };
 }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
-  final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  // data for testing
-  List<Client> testClients = [
-    Client(firstName: "Mickel", lastName: "Adeneye", blocked: false),
-    Client(firstName: "Samuel", lastName: "Adeniyi", blocked: true),
-    Client(firstName: "Daniel", lastName: "Adeola", blocked: false),
-  ];
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: FutureBuilder<List<Client>>(
-          future: DBProvider.db.getAllClients(),
-          builder: (BuildContext context, AsyncSnapshot<List<Client>> snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  Client item = snapshot.data[index];
-                  return Dismissible(
-                    key: UniqueKey(),
-                    background: Container(color: Colors.red),
-                    onDismissed: (direction) {
-                      DBProvider.db.deleteClient(item.id);
-                    },
-                    child: ListTile(
-                      title: Text(item.lastName),
-                      leading:Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(item.id.toString())
-                        ],
-                      ),
-                      trailing: Checkbox(
-                        onChanged: (bool value) {
-                          DBProvider.db.blockOrUnblock(item);
-                          setState(() {});
-                        },
-                        value: item.blocked,
-                      ),
-                    ),
-                  );
-                },
-              );
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          }
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () async {
-          Client rnd = testClients[math.Random().nextInt(testClients.length)];
-          await DBProvider.db.createClient(rnd);
-          setState(() {});
-        },
-      ),
-    );
-  }
-}
